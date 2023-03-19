f5ea032e9c3b0d475252d5d959fb5552ca10e49c
Aakash Kadam <aakashkadam360@gmail.com>
2023-03-19 16:02:13 +0530

    Cloud gateway master code
    Cloud gateway master code

---
+++ src/main/java/com/mycode/CloudGateway/controller/FallBackController.java
@@ -0,0 +1,26 @@
+package com.mycode.CloudGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallBack()
+    {
+        return "Order Service is Down!";
+    }
+
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallBack()
+    {
+        return "Payment Service is Down!";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallBack()
+    {
+        return "Product Service is Down!";
+    }
+}
